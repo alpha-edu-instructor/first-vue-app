82c15dc93e3dd4cf35b33b66099d1adef9e750ab
Adil Zhexenov <adilzhexenoff@gmail.com>
2025-03-31 21:37:09 +0500

    init project

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import Counter from "./components/Counter.vue";
+</script>
+
+<template>
+  <div class="container">
+    <h1 class="title text">Counters App</h1>
+    <div class="grid">
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  width: 1200px;
+  margin: 100px auto;
+}
+
+.grid {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 24px 40px;
+}
+
+.title {
+  font-size: 32px;
+  line-height: 40px;
+  font-weight: 700;
+  margin-bottom: 40px;
+}
+</style>
